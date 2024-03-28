6e0a4d391058f21fbf1067fb0da1c5d98cdd416b
Sean Parkin <sdparkin@protonmail.com>
2024-03-28 00:32:02 -0500

    Removed logging lines

--- src/server.ts
+++ src/server.ts
@@ -6,45 +6,33 @@ import { loggingHandler } from "./middleware/loggingHandler";
 import { corsHandler } from "./middleware/corsHandler";
 import { routeNotFound } from "./middleware/routeNotFound";
 import { server } from "./config/config";
+import mongoose from "mongoose";
+import "dotenv";
 
 export const application = express();
 export let httpServer: ReturnType<typeof http.createServer>;
 
 export const Main = () => {
-	logging.info("--------------------------------------------");
-	logging.info("Initializing API");
-	logging.info("--------------------------------------------");
 	application.use(express.urlencoded({ extended: true }));
 	application.use(express.json());
 
-	logging.info("----------------------------------------------");
-	logging.info("Logging and Configuration");
-	logging.info("----------------------------------------------");
 	application.use(loggingHandler);
 	application.use(corsHandler);
 
-	logging.info("----------------------------------------------");
-	logging.info("Define Controller Routing");
-	logging.info("----------------------------------------------");
 	application.get("/healthcheck", (req, res: Response) => {
 		return res.status(200).json({ message: "Server is running" });
 	});
 
-	logging.info("----------------------------------------------");
-	logging.info("Define Routing Error");
-	logging.info("----------------------------------------------");
 	application.use(routeNotFound);
 
-	logging.info("----------------------------------------------");
-	logging.info("Start Server");
-	logging.info("----------------------------------------------");
+	logging.info(process.env.MONGO_URI);
+	mongoose.connect(process.env.MONGO_URI || "");
+
 	httpServer = http.createServer(application);
 	httpServer.listen(server.SERVER_PORT, () => {
-		logging.info("----------------------------------------");
 		logging.info(
 			`Server started on ${server.SERVER_HOSTNAME}:${server.SERVER_PORT}`,
 		);
-		logging.info("----------------------------------------");
 	});
 };
 
