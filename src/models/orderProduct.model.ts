eb607582c51041687ed94775c0e087aca35ec70e
Sean Parkin <sdparkin@protonmail.com>
2024-04-03 00:10:42 -0500

    Created order product model

---
+++ src/models/orderProduct.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderProductSchema = new mongoose.Schema({
+  _id: Schema.Types.ObjectId,
+  orderId: {
+    type: Schema.Types.ObjectId,
+    ref: "Order",
+  },
+  productId: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantityOrdered: Number,
+  status: {
+    type: String,
+    enum: [
+      "created",
+      "picked",
+      "shipped",
+      "delivered",
+      "rejected",
+      "backordered",
+      "returned",
+      "credited",
+    ],
+    default: "created",
+  },
+});
+
+const OrderProduct = mongoose.model('OrderProduct', orderProductSchema)
+
+module.exports = OrderProduct
