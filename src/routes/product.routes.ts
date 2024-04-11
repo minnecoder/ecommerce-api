d2d660f43f0abe3c767d97b32ae3ae4bcc779672
Sean Parkin <sdparkin@protonmail.com>
2024-04-10 20:46:26 -0500

    Created product route'

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+} from "../controllers/product.controller";
+
+const router = express.Router();
+
+router.get("/", getAllProducts);
+router.get("/:id", getSingleProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
