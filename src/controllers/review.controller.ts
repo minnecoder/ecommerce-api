569c2af7648290521c6d664d5f22701e1e7fa1d6
Sean Parkin <sdparkin@protonmail.com>
2024-04-19 00:43:46 -0500

    Added checks to make sure that referenced ids are valid

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,109 +1,130 @@
 import { NextFunction, Request, Response } from "express";
 import { Review } from "../models/review.model";
+import { Product } from "../models/product.model";
+import { User } from "../models/user.model";
 
 export async function getAllReviews(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    try {
-        const allReviews = await Review.find();
-        return res.status(200).json({ data: allReviews });
-    } catch (error) {
-        return res.status(500).json({ message: "Server Error" });
-    }
+  try {
+    const allReviews = await Review.find();
+    return res.status(200).json({ data: allReviews });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function getSingleReview(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    try {
-        const ReviewId = req.params.id;
-        const findSingleReview = await Review.findOne({ _id: ReviewId });
+  try {
+    const ReviewId = req.params.id;
+    const findSingleReview = await Review.findOne({ _id: ReviewId });
 
-        if (!findSingleReview) {
-            return res
-                .status(404)
-                .json({ message: "Unable to find any data with that id" });
-        }
-
-        return res.status(200).json({ data: findSingleReview });
-    } catch (error) {
-        console.error("Error while finding review:", error);
-        return res.status(500).json({ message: "Server Error" });
+    if (!findSingleReview) {
+      return res
+        .status(404)
+        .json({ message: "Unable to find any data with that id" });
     }
+
+    return res.status(200).json({ data: findSingleReview });
+  } catch (error) {
+    console.error("Error while finding review:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function createReview(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const reviewData = req.body;
-    try {
-        const ReviewId = req.params.id;
-        const checkExisting = await Review.findOne({ _id: ReviewId });
-        if (checkExisting) {
-            return res.status(400).json({ message: "Review already exists" });
-        }
-        const newReview = new Review(reviewData);
-        await newReview.save();
+  const reviewData = req.body;
+  try {
+    const ReviewId = req.params.id;
 
-        return res
-            .status(201)
-            .json({ message: "Review created successfully", data: newReview });
-    } catch (error) {
-        console.error("Error  creating review:", error);
-        return res.status(500).json({ message: "Server Error" });
+    // Check if _id is already used
+    const checkExisting = await Review.findOne({ _id: ReviewId });
+    if (checkExisting) {
+      return res.status(400).json({ message: "Review already exists" });
     }
+
+    // Check if the Product _id is valid
+    const isProductIdValid = await Product.findOne({ _id: req.body.productId });
+    if (!isProductIdValid) {
+      return res
+        .status(400)
+        .json({ message: "The given product id is not valid" });
+    }
+
+    // Check if the User _id is valid
+    const isUserIdValid = await User.findOne({ _id: req.body.userId });
+    if (!isUserIdValid) {
+      return res
+        .status(400)
+        .json({ message: "The given user id is not valid" });
+    }
+
+    const newReview = new Review(reviewData);
+    await newReview.save();
+
+    return res
+      .status(201)
+      .json({ message: "Review created successfully", data: newReview });
+  } catch (error) {
+    console.error("Error  creating review:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function updateReview(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const reviewId = req.params.id;
-    const reviewFields = req.body;
+  const reviewId = req.params.id;
+  const reviewFields = req.body;
 
-    try {
-        const updateReview = await Review.findByIdAndUpdate(
-            { _id: reviewId },
-            { $set: reviewFields },
-            { new: true },
-        );
-        if (!updateReview) {
-            return res.status(404).json({ message: "Review not found" });
-        }
-
-        return res.status(200).json({
-            message: "Review updated successfully",
-            review: updateReview,
-        });
-    } catch (error) {
-        console.error("Error updating review:", error);
-        return res.status(500).json({ message: "Server Error" });
+  try {
+    const updateReview = await Review.findByIdAndUpdate(
+      { _id: reviewId },
+      { $set: reviewFields },
+      { new: true },
+    );
+    if (!updateReview) {
+      return res.status(404).json({ message: "Review not found" });
     }
+
+    return res.status(200).json({
+      message: "Review updated successfully",
+      review: updateReview,
+    });
+  } catch (error) {
+    console.error("Error updating review:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function deleteReview(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const reviewId = req.params.id;
-    try {
-        const deletedReview = await Review.findOneAndDelete({ _id: reviewId });
+  const reviewId = req.params.id;
+  try {
+    const deletedReview = await Review.findOneAndDelete({ _id: reviewId });
 
-        if (!deletedReview) {
-            return res.status(404).json({ message: "Review not found" });
-        }
-
-        return res.status(200).json({ message: "Review deleted successfully" });
-    } catch (error) {
-        console.error("Error deleting review:", error);
-        return res.status(500).json({ message: "Server Error" });
+    if (!deletedReview) {
+      return res.status(404).json({ message: "Review not found" });
     }
+
+    return res.status(200).json({ message: "Review deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting review:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
--- src/controllers/shipping.controller.ts
+++ src/controllers/shipping.controller.ts
@@ -1,109 +1,123 @@
 import { NextFunction, Request, Response } from "express";
 import { Shipping } from "../models/shipping.model";
+import { Order } from "../models/order.model";
 
 export async function getAllShippings(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    try {
-        const allShippings = await Shipping.find();
-        return res.status(200).json({ data: allShippings });
-    } catch (error) {
-        return res.status(500).json({ message: "Server Error" });
-    }
+  try {
+    const allShippings = await Shipping.find();
+    return res.status(200).json({ data: allShippings });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function getSingleShipping(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    try {
-        const ShippingId = req.params.id;
-        const findSingleShipping = await Shipping.findOne({ _id: ShippingId });
+  try {
+    const ShippingId = req.params.id;
+    const findSingleShipping = await Shipping.findOne({ _id: ShippingId });
 
-        if (!findSingleShipping) {
-            return res
-                .status(404)
-                .json({ message: "Unable to find any data with that id" });
-        }
-
-        return res.status(200).json({ data: findSingleShipping });
-    } catch (error) {
-        console.error("Error while finding shipping:", error);
-        return res.status(500).json({ message: "Server Error" });
+    if (!findSingleShipping) {
+      return res
+        .status(404)
+        .json({ message: "Unable to find any data with that id" });
     }
+
+    return res.status(200).json({ data: findSingleShipping });
+  } catch (error) {
+    console.error("Error while finding shipping:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function createShipping(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const shippingData = req.body;
-    try {
-        const ShippingId = req.params.id;
-        const checkExisting = await Shipping.findOne({ _id: ShippingId });
-        if (checkExisting) {
-            return res.status(400).json({ message: "Shipping already exists" });
-        }
-        const newShipping = new Shipping(shippingData);
-        await newShipping.save();
+  const shippingData = req.body;
+  try {
+    const ShippingId = req.params.id;
 
-        return res
-            .status(201)
-            .json({ message: "Shipping created successfully", data: newShipping });
-    } catch (error) {
-        console.error("Error  creating shipping:", error);
-        return res.status(500).json({ message: "Server Error" });
+    // Check if the id is already used
+    const checkExisting = await Shipping.findOne({ _id: ShippingId });
+    if (checkExisting) {
+      return res.status(400).json({ message: "Shipping already exists" });
     }
+
+    // Check if the Order _id is valid
+    const isOrderIdValid = await Order.findOne({ _id: req.body.orderId });
+    if (!isOrderIdValid) {
+      return res
+        .status(400)
+        .json({ message: "The given order id is not valid" });
+    }
+
+    const newShipping = new Shipping(shippingData);
+    await newShipping.save();
+
+    return res
+      .status(201)
+      .json({ message: "Shipping created successfully", data: newShipping });
+  } catch (error) {
+    console.error("Error  creating shipping:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function updateShipping(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const shippingId = req.params.id;
-    const shippingFields = req.body;
+  const shippingId = req.params.id;
+  const shippingFields = req.body;
 
-    try {
-        const updateShipping = await Shipping.findByIdAndUpdate(
-            { _id: shippingId },
-            { $set: shippingFields },
-            { new: true },
-        );
-        if (!updateShipping) {
-            return res.status(404).json({ message: "Shipping not found" });
-        }
-
-        return res.status(200).json({
-            message: "Shipping updated successfully",
-            shipping: updateShipping,
-        });
-    } catch (error) {
-        console.error("Error updating shipping:", error);
-        return res.status(500).json({ message: "Server Error" });
+  try {
+    const updateShipping = await Shipping.findByIdAndUpdate(
+      { _id: shippingId },
+      { $set: shippingFields },
+      { new: true },
+    );
+    if (!updateShipping) {
+      return res.status(404).json({ message: "Shipping not found" });
     }
+
+    return res.status(200).json({
+      message: "Shipping updated successfully",
+      shipping: updateShipping,
+    });
+  } catch (error) {
+    console.error("Error updating shipping:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
 
 export async function deleteShipping(
-    req: Request,
-    res: Response,
-    next: NextFunction,
+  req: Request,
+  res: Response,
+  next: NextFunction,
 ) {
-    const shippingId = req.params.id;
-    try {
-        const deletedShipping = await Shipping.findOneAndDelete({ _id: shippingId });
+  const shippingId = req.params.id;
+  try {
+    const deletedShipping = await Shipping.findOneAndDelete({
+      _id: shippingId,
+    });
 
-        if (!deletedShipping) {
-            return res.status(404).json({ message: "Shipping not found" });
-        }
-
-        return res.status(200).json({ message: "Shipping deleted successfully" });
-    } catch (error) {
-        console.error("Error deleting shipping:", error);
-        return res.status(500).json({ message: "Server Error" });
+    if (!deletedShipping) {
+      return res.status(404).json({ message: "Shipping not found" });
     }
+
+    return res.status(200).json({ message: "Shipping deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting shipping:", error);
+    return res.status(500).json({ message: "Server Error" });
+  }
 }
